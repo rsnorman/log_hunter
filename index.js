caf1207331da1332548c6e1a0ddf00c8fc397de4
Ryan Norman <rsnorman15@gmail.com>
2015-09-14 22:51:56 -0700

    Return line number and content of log calls
    Why
    ---
    Need to return line number and content of "console.log" calls so that a
    report can be generated
    
    This change addresses the need by
    ---------------------------------
    Loops through each line in the content and marks the line number and
    content for each "console.log" call

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-module.exports = function(content) {
-  var LOG_MATCHER = /console.log(.(call|apply))?\(.*\)(;)?/g;
-  content = content.replace(LOG_MATCHER, '');
-  return content;
+var LOG_MATCHER = /console.log(?:.(?:call|apply))?\(.*?\);?/g;
+
+function findAllCalls(content) {
+  var _i, _len, lines, matches, foundCalls;
+  foundCalls = [];
+  lines = content.split('\n');
+
+  for ( _i = 0, _len = lines.length; _i < _len; _i++ ) {
+    if ( matches = lines[_i].match(LOG_MATCHER) ) {
+      foundCalls = foundCalls.concat(matches.map(function(match) {
+        return {
+          lineNumber: _i + 1,
+          content: match
+        };
+      }));
+    }
+  }
+
+  return foundCalls;
+}
+
+function removeAllCalls(content) {
+  return content.replace(LOG_MATCHER, '');
+}
+
+module.exports = function(content, options) {
+  var removeCalls, foundCalls;
+  options = options || {};
+  removeCalls = options.remove !== false;
+
+  foundCalls = findAllCalls(content);
+
+  if ( removeCalls ) {
+    content = removeAllCalls(content);
+  }
+
+  return {
+    content: content,
+    lines: foundCalls
+  };
 };
