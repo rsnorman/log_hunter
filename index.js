efe8c2dc92bac4507ff0c3efaa58b808fca127e8
Ryan Norman <rsnorman15@gmail.com>
2015-09-23 17:43:41 -0700

    Use loader from demo
    Why
    ---
    The loader was taking on a lot of responsibilities so decided to just
    focus on finding and warning about "console.log" statements
    
    This change addresses the need by
    ---------------------------------
    Deleted libs for removing console.log statements

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 /*!
- * LogHunterLoader
+ * LogMapperLoader
  * Copyright(c) 2015 Ryan Scott Norman
  * MIT Licensed
  */
 
 'use strict';
 
-var LogHunter = require('./lib/log_hunter');
+var logMapper = require('./lib/mapper');
+var LogExceptions = require('./lib/exceptions');
+var path = require('path');
 var loaderUtils = require('loader-utils');
+
+var EXCEPTIONS_PATH = path.resolve('./log-exceptions.json');
 
 /**
  * Creates a warning message for all "console.log" occurrences
@@ -17,32 +21,100 @@ var loaderUtils = require('loader-utils');
  */
 function createWarningMessage(logOccurrences) {
   var message;
+
   message = 'Found ' + logOccurrences.length + ' "console.log" statements\n'
 
   return logOccurrences.reduce(function(msg, occurrence) {
-    return msg + '  `' + occurrence.content + '` on line ' + occurrence.lineNumber + '\n';
+    return msg + '  "' + occurrence.content + '" on line ' + occurrence.lineNumber + '\n';
   }, message);
 }
 
-/**
- * Loader for finding and/or removing "console.log" statements
- *
- * @param {String} content with potential "console.log" statements
- * @return {String} original content with or without "console.log" statements
- */
-function LogHunterLoader(content) {
-  var config, logMatchResults, emitter;
-  config = loaderUtils.parseQuery(this.query);
+function removeExceptions(logOccurrences, exceptions) {
+  return logOccurrences.filter(function(occurrence) {
+    return exceptions.indexOf(occurrence.lineNumber) === -1;
+  })
+}
 
-  logMatchResults = new LogHunter(content, !!config.remove);
+function getLogOccurrencesSync(content, exceptionFile) {
+  var skipLines, logExceptions, logOccurrences;
+
+  if (exceptionFile) {
+    logExceptions = new LogExceptions(exceptionFile);
+    skipLines = logExceptions.forResource(this.resource);
+  } else {
+    skipLines = [];
+  }
+
+  logOccurrences = logMapper(content);
+  return removeExceptions(logOccurrences, skipLines);
+}
+
+function getLogOccurrences(content, exceptionFile, callback) {
+  var skipLines, logExceptions, logOccurrences;
+
+  if (callback) {
+    logOccurrences = logMapper(content);
+
+    if (exceptionFile) {
+      logExceptions = new LogExceptions(exceptionFile);
+      logExceptions.forResourceAsync(this.resource).then(function(skipLines) {
+        callback(null, removeExceptions(logOccurrences, skipLines));
+      });
+    } else {
+      callback(null, logOccurrences);
+    }
+  } else {
+    return getLogOccurrencesSync.call(this, content, exceptionFile);
+  }
+}
+
+function commentContent(content, message) {
+  var commentWarning;
+  commentWarning = message.split('\n').map(function(line) {
+    return '// ' + line;
+  }).join('\n');
+
+  return commentWarning + '\n\n' + content;
+}
+
+function LogHunterLoader(content) {
+  var logOccurrences, warningMessage, emitter, config, callback;
+
+  this.cacheable && this.cacheable();
+
+  config = loaderUtils.parseQuery(this.query);
 
   emitter = config.emitError ? this.emitError : this.emitWarning;
 
-  if ( !!emitter && logMatchResults.occurrences.length > 0 ) {
-    emitter(createWarningMessage(logMatchResults.occurrences));
-  }
+  this.addDependency(EXCEPTIONS_PATH);
 
-  return logMatchResults.content;
+  callback = this.async();
+
+  if (callback) {
+    getLogOccurrences.call(this, content, EXCEPTIONS_PATH, function(err, logOccurrences) {
+      warningMessage = createWarningMessage(logOccurrences);
+
+      if (logOccurrences.length > 0) {
+        emitter && emitter(warningMessage);
+
+        callback(null, commentContent(content, warningMessage));
+      } else {
+        callback(null, content);
+      }
+    });
+  } else {
+    logOccurrences = getLogOccurrences.call(this, content, EXCEPTIONS_PATH);
+
+    warningMessage = createWarningMessage(logOccurrences);
+
+    if (logOccurrences.length > 0) {
+      emitter && emitter(warningMessage);
+
+      return commentContent(content, warningMessage);
+    } else {
+      return content;
+    }
+  }
 }
 
 module.exports = LogHunterLoader;
